2cc9e12047b04956954d5041c5a604a01c8038e6
xiaoconghu <huxc10086@163.com>
2018-09-02 18:30:31 +0800

    进入控制器按钮组件

---
+++ component/music-control/music-control.js
@@ -0,0 +1,57 @@
+// component/music-control/music-control.js
+import Song from "../../utils/song";
+
+const app = getApp();
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      isPlay:true
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    click(){
+      if (app.globalData.songList.length > 0){
+        wx.navigateTo({
+          url: '../player/player',
+        })
+      }
+      
+    }
+  },
+    pageLifetimes:{
+        show: function () {
+            console.log(111);
+        },
+        hidden:function () {
+            console.log(111);
+        }
+    },
+    ready() {
+
+        // if(app.globalData.songList.length>0){
+        //     this.setData({isPlay:true})
+        // }else {
+        //     this.setData({isPlay:false})
+        // }
+    },
+    attached(){
+
+        // if(app.globalData.songList.length>0){
+        //     this.setData({isPlay:true})
+        // }else {
+        //     this.setData({isPlay:false})
+        // }
+    }
+})
